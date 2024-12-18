cb36cfb1c0e596ae56e37035a91dc315e1c2ddb9
brookdb <brksolomon@gmail.com>
2024-12-18 17:30:26 +0300

    timeline activity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Rectangles from "./components/rectangles";
 import gsap from "gsap";
+import Stalactite from "./components/stalactite";
 import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
@@ -8,8 +8,7 @@ gsap.registerPlugin(useGSAP);
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Welcome to GSAP Animations!</h1>
-      <Rectangles />
+      <Stalactite />
     </div>
   );
 }
---
+++ src/components/stalactite.js
@@ -0,0 +1,36 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+export default function Stalactite() {
+  const stalactiteImage =
+    "https://www.clker.com/cliparts/V/u/Z/V/e/L/stalactites-md.png";
+  useGSAP(() => {
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+    //shaking animation
+    tl.fromTo(
+      ".stalactite",
+      { y: 0, opacity: 1 },
+      {
+        x: -10,
+        duration: 0.1,
+        repeat: 5,
+        yoyo: true,
+        ease: "power1.inOut",
+      }
+    ); //add second tween here to make it fal
+  });
+
+  return (
+    <div className="cave-container">
+      <div
+        className="stalactite"
+        style={{
+          backgroundImage: `url(${stalactiteImage})`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </div>
+  );
+}
